3cb84198791509fb7edf7173df69cf2de1849c5b
sudeep500 <sudipta13.chakraborty@gmail.com>
2022-09-20 03:12:59 +0530

    Doubly LL Deletion Operations

---
+++ LinkedListOperations/DoublyLLDeleteBetween.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    struct node *prev;
+    int data;
+    struct node *next;
+};
+
+void LinkedListTraversal(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element:%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct node *DoublyLLDeleteFirst(struct node *head, int index)
+{
+    struct node *p = head;
+    int i = 0;
+    while (i != index)
+    {
+        p = p->next;
+        i++;
+    }
+    p->prev->next = p->next;
+    p->next->prev = p->prev;
+    free(p);
+    return head;
+}
+
+int main()
+{
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+
+    // Allocated memory for nodes in the linked list in heap
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+
+    // Linked First and Second node
+    head->prev = NULL;
+    head->data = 7;
+    head->next = second;
+
+    // Linked second and Third node
+    second->prev = head;
+    second->data = 11;
+    second->next = third;
+
+    // Linked Third and Fourth node
+    third->prev = second;
+    third->data = 23;
+    third->next = fourth;
+
+    // Terminates the list at the Fourth node
+    fourth->prev = third;
+    fourth->data = 33;
+    fourth->next = NULL;
+
+    printf("Linked list before deletion:\n");
+    LinkedListTraversal(head);
+    head = DoublyLLDeleteFirst(head, 2);
+    printf("Linked list after deletion:\n");
+    LinkedListTraversal(head);
+    return 0;
+}
---
+++ LinkedListOperations/DoublyLLDeleteLast.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    struct node *prev;
+    int data;
+    struct node *next;
+};
+
+void LinkedListTraversal(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element:%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct node *DoublyLLDeleteLast(struct node *head)
+{
+    struct node *p = head;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->prev->next = NULL;
+    free(p);
+    return head;
+}
+
+int main()
+{
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+
+    // Allocated memory for nodes in the linked list in heap
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+
+    // Linked First and Second node
+    head->prev = NULL;
+    head->data = 7;
+    head->next = second;
+
+    // Linked second and Third node
+    second->prev = head;
+    second->data = 11;
+    second->next = third;
+
+    // Linked Third and Fourth node
+    third->prev = second;
+    third->data = 23;
+    third->next = fourth;
+
+    // Terminates the list at the Fourth node
+    fourth->prev = third;
+    fourth->data = 33;
+    fourth->next = NULL;
+
+    printf("Linked list before deletion:\n");
+    LinkedListTraversal(head);
+    head = DoublyLLDeleteLast(head);
+    printf("Linked list after deletion:\n");
+    LinkedListTraversal(head);
+    return 0;
+}
