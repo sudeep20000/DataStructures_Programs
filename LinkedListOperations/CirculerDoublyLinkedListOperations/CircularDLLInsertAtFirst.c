d92e734c045b125feb61cbf562c551b115b320f5
sudeep500 <sudipta13.chakraborty@gmail.com>
2022-09-21 01:36:58 +0530

    Each type of LL is in the Separate Folders

---
+++ LinkedListOperations/CirculerDoublyLinkedListOperations/CircularDLLInsertAtFirst.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    struct node *prev;
+    int data;
+    struct node *next;
+};
+
+void LinkedListTraversal(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element:%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct node *CircularDLLInsertFirst(struct node *head, int data)
+{
+    struct node *p = (struct node *)malloc(sizeof(struct node));
+    struct node *ptr = head->next;
+    while (ptr->next != head)
+    {
+        ptr = ptr->next;
+    }
+    p->data = data;
+    p->next = ptr->next;
+    ptr->next = p;
+    p->prev = ptr;
+    head = p;
+    return head;
+}
+
+int main()
+{
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+
+    // Allocated memory for nodes in the linked list in heap
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+
+    // Linked First and Second node
+    head->prev = fourth;
+    head->data = 7;
+    head->next = second;
+
+    // Linked second and Third node
+    second->prev = head;
+    second->data = 11;
+    second->next = third;
+
+    // Linked Third and Fourth node
+    third->prev = second;
+    third->data = 23;
+    third->next = fourth;
+
+    // Terminates the list at the Fourth node
+    fourth->prev = third;
+    fourth->data = 33;
+    fourth->next = head;
+
+    printf("Linked list before insertion:\n");
+    LinkedListTraversal(head);
+    head = CircularDLLInsertFirst(head, 3);
+    printf("Linked list after insertion:\n");
+    LinkedListTraversal(head);
+    return 0;
+}
