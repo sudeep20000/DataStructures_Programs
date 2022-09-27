993508fd0fd8131a506aca2b400b9586c21b693a
Sudipta Chakraborty <sudipta13.chakraborty@gmail.com>
2022-09-28 01:38:53 +0530

    Initial Commit

---
+++ StackOperations/ParenthesisMatchingProblem.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int top;
+    int size;
+    char *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void push(struct stack *ptr, char value)
+{
+    if (isFull(ptr))
+    {
+        printf("Stack overflow!!! you can't push %d in stack\n", value);
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = value;
+    }
+}
+
+char pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("Stack underflow!!!\n");
+    }
+    else
+    {
+        char val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+int parenthisisMatch(char *exp)
+{
+    struct stack *sp;
+    sp->size = 100;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size * sizeof(char));
+
+    for (int i = 0; i < exp[i] != '\0'; i++)
+    {
+        if (exp[i] == '(')
+        {
+            push(sp, exp[i]);
+        }
+        else if (exp[i] == ')')
+        {
+            if (isEmpty(sp))
+                return 0;
+
+            pop(sp);
+        }
+    }
+    if (isEmpty(sp))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    char *exp = "(3*1)(1-1)(9+1)";
+    if (parenthisisMatch(exp))
+    {
+        printf("The parenthesis is matching\n");
+    }
+    else
+    {
+        printf("The parenthesis is not matching ");
+    }
+    return 0;
+}
