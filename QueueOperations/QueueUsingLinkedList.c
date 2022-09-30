273a0b37730e485c6bb3dc24612afcfc615eff56
Sudipta Chakraborty <sudipta13.chakraborty@gmail.com>
2022-10-01 02:32:47 +0530

    Queue using LinkedList

---
+++ QueueOperations/QueueUsingLinkedList.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node *f = NULL;
+struct node *r = NULL;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void LinkedListTraversal(struct node *ptr)
+{
+    printf("printing the element of this linkedlist:\n");
+    while (ptr != NULL)
+    {
+        printf("Element:%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+void enqueue(int val)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node));
+    if (n == NULL)
+    {
+        printf("Queue is Full\n");
+    }
+    else
+    {
+        n->data = val;
+        n->next = NULL;
+        if (f == NULL)
+        {
+            f = r = n;
+        }
+        r->next = n;
+        r = n;
+    }
+}
+
+int dequeue()
+{
+    int val = -1;
+    struct node *ptr = f;
+    if (f == NULL)
+    {
+        printf("Queue is Empty\n");
+    }
+    else
+    {
+        f = f->next;
+        val = ptr->data;
+        free(ptr);
+    }
+    return val;
+}
+
+int main()
+{
+    LinkedListTraversal(f);
+    enqueue(34);
+    enqueue(44);
+    enqueue(54);
+    enqueue(64);
+    printf("Dequeuing element %d\n", dequeue());
+    printf("Dequeuing element %d\n", dequeue());
+    printf("Dequeuing element %d\n", dequeue());
+    printf("Dequeuing element %d\n", dequeue());
+    LinkedListTraversal(f);
+
+    return 0;
+}
