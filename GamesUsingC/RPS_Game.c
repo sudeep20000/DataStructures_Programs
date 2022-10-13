6d520426b3165f420a5057e404bba9ec72fefc98
Sudipta Chakraborty <sudipta13.chakraborty@gmail.com>
2022-10-14 03:28:32 +0530

    Improve the code of the game

--- GamesUsingC/RPS_Game.c
+++ GamesUsingC/RPS_Game.c
@@ -15,23 +15,25 @@ int main()
 int main()
 {
     char name[10];
-    int i = 0;
+    int i = 0, j;
     printf("\t----Welcome To RockPaperScissors Game----\t\n");
     printf("Enter your name:");
     scanf("%s", &name);
     int u_count = 0;
     int c_count = 0;
-    int round = 1;
-    while (i < 4)
+    int round = 0;
+    printf("Enter how many rounds you want to play this game:");
+    scanf("%d", &j);
+    while (j--)
     {
         int n, r;
-        printf("\t------(Round %d)------\t\n", round);
+        printf("\t------(Round %d)------\t\n", round + 1);
         printf("%s your turn=>\n", name);
     end:
-        printf("Enter 0 for rock\t1 for paper\t2 for scissors\n");
+        printf("Enter 1 for rock\t2 for paper\t3 for scissors: ");
         scanf("%d", &n);
         printf("You Entered: %d\n", n);
-        switch (n)
+        switch ((n - 1))
         {
         case 0:
             printf("You choose Rock\n");
@@ -46,11 +48,13 @@ int main()
             printf("%s you entered %d which is not valid input! Plz Select only those inputs which is given in the option.\n", name, n);
             goto end;
         }
+        printf("\n");
         printf("Computer's turn=>\n");
-        printf("Enter 0 for rock\t1 for paper\t2 for scissors\n");
+        printf("Enter 1 for rock\t2 for paper\t3 for scissors\n");
         srand(time(NULL));
         r = rand() % 2;
-        printf("Computer Entered: %d\n", r);
+        printf("Computer Entered: %d\n", r + 1);
+        // printf("value of r=%d\n", r);
         switch (r)
         {
         case 0:
@@ -63,12 +67,14 @@ int main()
             printf("Computer choose Scissors\n");
             break;
         }
-        if (n == 0 && r == 2 || n == 1 && r == 0 || n == 2 && r == 1)
+        // printf("value of n=%d\n", n);
+        // printf("value of r=%d\n", r);
+        if ((n - 1) == 0 && r == 2 || (n - 1) == 1 && r == 0 || (n - 1) == 2 && r == 1)
         {
             printf("Congratulatuion %s you won!!!!\n", name);
             u_count++;
         }
-        else if (n == r)
+        else if ((n - 1) == r)
         {
             printf("Result ties!!!!\n");
         }
